b9a692a8c967fa556f54931098c7a2911175c07c
zhangyu <purpose_iris@163.com>
2019-05-05 15:25:39 +0800

    界面的重新修改

--- src/components/Tickets.vue
+++ src/components/Tickets.vue
@@ -29,36 +29,35 @@
                     <template slot="prepend">终点站：</template>
                   </el-input>
                 </el-col>
+                <el-col :span="2" :offset="2">
+                  <el-button round @click="query()">查询</el-button>
+                </el-col>
               </el-row>
             </template>
           </el-row>
           <el-row>
-            <el-col :span="2" :offset="18">
-              <el-button round @click="query()">查询</el-button>
-            </el-col>
-          </el-row>
-          <el-row>
-            <el-table border :data="OriginalTickets.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 100%;;">
+            <!--.slice((currentPage-1)*pagesize,currentPage*pagesize)-->
+            <el-table border :data="OriginalTickets.slice((currentPage-1)*pagesize,currentPage*pagesize)" style="width: 100%;">
               <template v-for="tableProp in tableProps">
                 <el-table-column sortable :prop="tableProp.props" :label="tableProp.label" :key="tableProp.index">
                 </el-table-column>
               </template>
               <el-table-column  width="125px">
                 <template slot-scope="scope">
-                  <el-button round size="mini" icon="el-icon-delete" @click="delPro(scope.row.stanid)">购票</el-button>
+                  <el-button round size="mini" icon="el-icon-date" @click="buy(scope.row.tid)">购票</el-button>
                 </template>
               </el-table-column>
             </el-table>
-          </el-row>
-          <el-row>
-            <el-pagination
-              background
-              @current-change="handleCurrentChange"
-              layout="total, sizes, prev, pager, next, jumper"
-              :total="total">
+            <el-pagination class="fy"
+                           layout="prev, pager, next"
+                           @current-change="current_change"
+                           :total="total"
+                           background
+            >
             </el-pagination>
           </el-row>
         </el-main>
+        {{'长度'+total}}
       </el-container>
     </div>
 </template>
@@ -78,6 +77,9 @@
                 tableData:[],
                 tableProps:[],
                 propsDefault:[{
+                  props:'tid',
+                  label:'编号'
+                },{
                   props:'tname',
                   label:'车次'
                 },{
@@ -109,13 +111,15 @@
         this.total=this.OriginalTickets.length;
       },
       method:{
-        handleCurrentChange(currentPage){
+        current_change:function(currentPage){
           this.currentPage = currentPage;
         },
-        query(){
+        query:function(){
           //车次查询
           if(this.queryParam==1){
-
+            ticket.findByTname().then(data=>{
+              this.OriginalTickets = data;
+            });
             //车站查询
           }else{
 
@@ -126,5 +130,8 @@
 </script>
 
 <style scoped>
-
+  .fy{
+    text-align:center;
+    margin-top:30px;
+  }
 </style>
